ee2f993fb52853453e99cbb9efd30c6a1a66b4ec
Patrick Lebold <prlebold@wpi.edu>
2016-03-01 22:39:53 -0500

    Comments

--- heaney/lebold/bayesnet/NodeType.java
+++ heaney/lebold/bayesnet/NodeType.java
@@ -1,5 +1,9 @@
 package heaney.lebold.bayesnet;
 
+/* Type: public enum NodeType
+ * 	- Extends: Object
+ * 	- Description: This enum represents a classification of a Node
+ */
 public enum NodeType {
 	TRUE,
 	FALSE,
